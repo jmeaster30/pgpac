5e11d03b58a0339fa7b4d21a83df539dd95df675
jmeaster30 <jmeasterday@comcast.net>
2023-01-29 15:20:22 -0500

    Started planning the configuration file based on how I want it.

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/pganalyze/pg_query_go/v2 v2.2.0 // indirect
 	google.golang.org/protobuf v1.23.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -10,11 +10,15 @@ import (
 
 func main() {
 
-	target := flag.String("target", ".", "The target folder or file.")
-	server := flag.String("server", "", "The server to deploy the pgpac file to.")
-	database := flag.String("db", "", "The database to deploy the pgpac file to.")
-	username := flag.String("user", "", "The user to use for authentication with the database server.")
-	password := flag.String("pass", "", "The password to use for authentication with the database server.")
+	config_param := flag.String("config", "pgpac.conf", "The file to pull the configuration from.")
+	target_param := flag.String("target", ".", "The target folder or file.")
+	server_param := flag.String("server", "", "The server from the config file to deploy the pgpac file to.")
+	project_param := flag.String("project", "", "The project from the config file to package into the pgpac file.")
+	host_param := flag.String("host", "", "The host server to deploy to.")
+	port_param := flag.String("port", "", "The port of the server to deploy to")
+	database_param := flag.String("db", "", "The database to deploy to.")
+	username_param := flag.String("user", "", "The user to use for authentication with the database server.")
+	password_param := flag.String("pass", "", "The password to use for authentication with the database server.")
 
 	flag.Parse()
 
@@ -24,11 +28,15 @@ func main() {
 	}
 
 	commandName := flag.Args()[0]
-	_ = *target
-	_ = *server
-	_ = *database
-	_ = *username
-	_ = *password
+	config = *config_param
+	target = *target_param
+	server = *server_param
+	project = *project_param
+	host = *host_param
+	port = *port_param
+	database = *database_param
+	username = *username_param
+	password = *password_param
 
 	switch commandName {
 	case "help":
@@ -46,7 +54,7 @@ func main() {
 	case "deploy":
 		break
 	default:
-		fmt.Printf("Unknown command '%s'. Expected init, package, or deploy.", commandName)
+		fmt.Printf("Unknown command '%s'. Expected init, package, deploy, or help.", commandName)
 		os.Exit(1)
 	}
 
